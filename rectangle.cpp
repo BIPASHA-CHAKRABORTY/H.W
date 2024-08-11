1a602858e85bff53ec12c32f551c0ca626cf4d7d
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-08-11 23:05:24 +0530

    Create rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class rectangle
+{
+  private:
+      int L, B, A;
+  public:
+      void input(int length, int breadth)
+      {
+          L = length;
+          B = breadth;
+      }
+
+      void Area()
+      {
+          A = L * B;
+      }
+
+      
+      void display() 
+      {
+          cout << "Value of Length: " << L << endl;
+          cout << "Value of Breadth: " << B << endl;
+          cout << "Area of Rectangle: " << A << endl;
+      }
+};
+
+int main()
+{
+    rectangle R;
+    
+    
+    R.input(4, 5);
+    
+    
+    R.Area();
+    
+    
+    R.display();
+
+    return 0;
+}
