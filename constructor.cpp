47fea511eea99a054f7356391102527dd03cc581
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-08-12 23:21:25 +0530

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,37 @@
+// C++ Program to illustrate how to define the parameterized
+// constructor outside the class
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// class definition
+class student {
+    int rno;
+    char name[50];
+    double fee;
+
+public:
+    student(int, char[], double);
+    void display();
+};
+
+// parameterized constructor outside class
+student::student(int no, char n[], double f)
+{
+    rno = no;
+    strcpy(name, n);
+    fee = f;
+}
+
+void student::display()
+{
+    cout << endl << rno << "\t" << name << "\t" << fee;
+}
+
+// driver code
+int main()
+{
+    student s(1001, "Ram", 10000);
+    s.display();
+    return 0;
+}
